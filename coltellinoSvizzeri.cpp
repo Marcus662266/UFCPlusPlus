b5915d6a883588fbab70399d4d3f38e505166257
Marcus662266 <124903413+Marcus662266@users.noreply.github.com>
2023-02-15 09:35:15 +0100

    stampa numero pari con bool

--- coltellinoSvizzeri.cpp
+++ coltellinoSvizzeri.cpp
@@ -9,7 +9,29 @@ int piuGrande(int x,int y){
 
 }
 
-bool isEven(int x){}
+bool isEven(int x)
+{
+
+     bool isEven(int x) {
+    if (x % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+     int main() {
+    int n;
+    cout << "Inserisci un numero: ";
+    cin >> n;
+    if (isEven(n)) {
+        cout << n << " e' un numero pari." << endl;
+    } else {
+        cout << n << " e' un numero dispari." << endl;
+    }
+    return 0;
+}
+}
 
 double costoTotale(double prezzo, int interesse){}
 
---
+++ stampa-seun-numero-pari.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+bool isEven(int x) {
+    if (x % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Inserisci un numero: ";
+    cin >> n;
+    if (isEven(n)) {
+        cout << n << " e' un numero pari." << endl;
+    } else {
+        cout << n << " e' un numero dispari." << endl;
+    }
+    return 0;
+}
